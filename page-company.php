70c96c1f4e5c47e90995cd2e97701e6639f32713
shika488 <>
2023-08-02 18:37:01 +0900

    会社概要ページを修正する

--- page-company.php
+++ page-company.php
@@ -6,33 +6,34 @@ Template Name:会社概要
 
 <?php get_header();?>
 
-    <section>
-
-        <table>
-            <tr>
-                <td class="">会社名</td>
-                <td class=""><?php echo CFS()->get('name'); ?></td>
-            </tr>
-            <tr>
-                <td class="">設立</td>
-                <td class=""><?php echo CFS()->get('found'); ?></td>
-            </tr>
-            <tr>
-                <td class="">代表者</td>
-                <td class=""><?php echo CFS()->get('CEO'); ?></td>
-            </tr>
-            <tr>
-                <td class="">事業内容</td>
-                <td class=""><?php echo CFS()->get('business-details'); ?></td>
-            </tr>
-            <tr>
-                <td class="">所在地</td>
-                <td class=""><?php echo CFS()->get('address'); ?></td>
-            </tr>
-            <tr>
-                <td class="">連絡先</td>
-                <td class=""><?php echo CFS()->get('tel'); ?></td>
-            </tr>
+    <section class="w-[97%] md:w-[90%] my-8">
+        <table class="mx-auto w-[90%] h-[300px] border-none">
+            <tbody class="border-none">
+                <tr class="w-full table-row border-none">
+                    <th class="left w-1/2">会社名</th>
+                    <td class="right w-1/2"><?php echo CFS()->get('name'); ?></td>
+                </tr>
+                <tr class="w-full table-row border-none">
+                    <th class="left">設立</th>
+                    <td class="right"><?php echo CFS()->get('found'); ?></td>
+                </tr>
+                <tr class="w-full table-row border-none">
+                    <th class="left">代表者</th>
+                    <td class="right"><?php echo CFS()->get('CEO'); ?></td>
+                </tr>
+                <tr class="w-full table-row border-none">
+                    <th class="left">事業内容</th>
+                    <td class="right"><?php echo CFS()->get('business-details'); ?></td>
+                </tr>
+                <tr class="w-full table-row border-none">
+                    <th class="left">所在地</th>
+                    <td class="right"><?php echo CFS()->get('address'); ?></td>
+                </tr>
+                <tr class="w-full table-row border-none">
+                    <th class="left">連絡先</th>
+                    <td class="right"><?php echo CFS()->get('tel'); ?></td>
+                </tr>
+            </tbody>
         </table>
     </section>
 
