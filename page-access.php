dca1999f7d15467e47b681fdb0ce89b05a998f5c
shika488 <>
2023-08-02 22:33:14 +0900

    アクセスページを修正する
    テキストと地図の表示を左右（上下）入れ替える

--- page-access.php
+++ page-access.php
@@ -6,35 +6,25 @@ Template Name:アクセス
 
 <?php get_header();?>
 
-    <section class="w-[97%] md:w-[90%] my-5 md:flex justify-between items-center">
+    <section class="w-[97%] lg:w-[90%] mb-5 md:my-5 md:flex justify-between items-center">
+
+        <dl class="md:w-[50%] lg:h-[350px] mb-4 md:mb-0 md:mr-5 flex flex-wrap items-center">
+            <dt class="left">住所</dt>
+            <dd class="right"><?php echo CFS()->get('address', 9); ?></dd>
+            <dt class="left">連絡先</dt>
+            <dd class="right"><?php echo CFS()->get('tel', 9); ?></dd>
+            <dt class="left">最寄り駅</dt>
+            <dd class="right"><?php echo CFS()->get('nearest'); ?></dd>
+            <dt class="left">営業時間</dt>
+            <dd class="right"><?php echo CFS()->get('open'); ?></dd>
+            <dt class="left">定休日</dt>
+            <dd class="right"><?php echo CFS()->get('close'); ?></dd>
+        </dl>
+
         <div class="md:w-[50%]">
             <div class="w-full h-full relative pt-[75%]"><?php echo CFS()->get('map'); ?></div>
         </div>
-        
-        <table class="mt-5 md:mt-0 md:w-[48%] h-72 md:h-[350px] text-sm md:text-base border-none">
-            <tbody class="border-none">
-                <tr class="border-none">
-                    <th class="left">住所</th>
-                    <td class="right"><?php echo CFS()->get('address', 9); ?></td>
-                </tr>
-                <tr class="border-none">
-                    <th class="left">連絡先</th>
-                    <td class="right"><?php echo CFS()->get('tel', 9); ?></td>
-                </tr>
-                <tr class="border-none">
-                    <th class="left">最寄り駅</th>
-                    <td class="right text-base"><?php echo CFS()->get('nearest'); ?></td>
-                </tr>
-                <tr class="border-none">
-                    <th class="left">営業時間</th>
-                    <td class="right text-base"><?php echo CFS()->get('open'); ?></td>
-                </tr>
-                <tr class="border-none">
-                    <th class="left">定休日</th>
-                    <td class="right text-base"><?php echo CFS()->get('close'); ?></td>
-                </tr>
-            </tbody>
-        </table>
+
     </section>
 
 <?php get_footer(); ?>
