07642af6708fccd1ed4489a7379b5e8026de4e1d
shika488 <>
2023-08-03 22:52:15 +0900

    お問い合わせページを修正する
    チェックボックスを横一列に表示させる
    郵便番号から住所を自動入力させる（プラグイン:YubinBango）

--- page-contact.php
+++ page-contact.php
@@ -7,7 +7,7 @@ Template Name:お問い合わせ
 <?php get_header();?>
 
     <section class="mx-auto w-[95%] md:w-[90%]">
-            <?php echo do_shortcode('[contact-form-7 id="138" title="Contact"]'); ?>
+        <?php echo do_shortcode('[contact-form-7 id="138" title="Contact" html_class="h-adr"]'); ?>
     </section>
 
 <?php get_footer(); ?>
