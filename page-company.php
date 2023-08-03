8f9e2fb201b6e02cc81645a70d1e9c4be7695721
shika488 <>
2023-08-03 16:40:03 +0900

    会社概要ページを修正する
    表を中央寄せにする

--- page-company.php
+++ page-company.php
@@ -7,34 +7,22 @@ Template Name:会社概要
 <?php get_header();?>
 
     <section class="w-[97%] md:w-[90%] my-8">
-        <table class="mx-auto w-[90%] h-[300px] border-none">
-            <tbody class="border-none">
-                <tr class="w-full table-row border-none">
-                    <th class="left w-1/2">会社名</th>
-                    <td class="right w-1/2"><?php echo CFS()->get('name'); ?></td>
-                </tr>
-                <tr class="w-full table-row border-none">
-                    <th class="left">設立</th>
-                    <td class="right"><?php echo CFS()->get('found'); ?></td>
-                </tr>
-                <tr class="w-full table-row border-none">
-                    <th class="left">代表者</th>
-                    <td class="right"><?php echo CFS()->get('CEO'); ?></td>
-                </tr>
-                <tr class="w-full table-row border-none">
-                    <th class="left">事業内容</th>
-                    <td class="right"><?php echo CFS()->get('business-details'); ?></td>
-                </tr>
-                <tr class="w-full table-row border-none">
-                    <th class="left">所在地</th>
-                    <td class="right"><?php echo CFS()->get('address'); ?></td>
-                </tr>
-                <tr class="w-full table-row border-none">
-                    <th class="left">連絡先</th>
-                    <td class="right"><?php echo CFS()->get('tel'); ?></td>
-                </tr>
-            </tbody>
-        </table>
+
+        <dl class="h-[300px] flex flex-wrap items-center">
+            <dt class="left w-1/2">会社名</dt>
+            <dd class="right w-1/2"><?php echo CFS()->get('name'); ?></dd>
+            <dt class="left w-1/2">設立</dt>
+            <dd class="right w-1/2"><?php echo CFS()->get('found'); ?></dd>
+            <dt class="left w-1/2">代表者</dt>
+            <dd class="right w-1/2"><?php echo CFS()->get('CEO'); ?></dd>
+            <dt class="left w-1/2">事業内容</dt>
+            <dd class="right w-1/2"><?php echo CFS()->get('business-details'); ?></dd>
+            <dt class="left w-1/2">所在地</dt>
+            <dd class="right w-1/2"><?php echo CFS()->get('address'); ?></dd>
+            <dt class="left w-1/2">連絡先</dt>
+            <dd class="right w-1/2"><?php echo CFS()->get('tel'); ?></dd>
+        </dl>
+
     </section>
 
 <?php get_footer(); ?>
