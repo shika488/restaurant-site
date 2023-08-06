12d4bafe7ed5bfc94ff5d269c192cabcfeb91b94
shika488 <>
2023-08-06 14:42:31 +0900

    トップページの表示を修正する
    ヘッダー・フッターを修正

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     </main>
 
-    <footer class="w-full h-10 bg-black text-white">
-        <small>© <span class="font-title tracking-widest"><?php bloginfo('name'); ?></span> All Rights Reserved.</small>
+    <footer class="w-full h-10 bg-[#ff0a91] text-white">
+        <small class="my-3">© <span class="font-title tracking-widest"><?php bloginfo('name'); ?></span> All Rights Reserved.</small>
     </footer>
 
     <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -9,47 +9,47 @@
 </head>
 <body class="min-h-screen flex flex-col items-center text-center font-body font-light">
     <header class="w-full">
-        <div class="md:mt-0 px-2 md:px-0 w-full h-20 md:h-14 md:flex justify-around items-center">
-            <h1 class="my-2 md:my-0 font-title text-4xl tracking-widest">
+        <div class="md:mt-0 px-2 md:px-0 w-full h-28 md:h-32 lg:flex justify-around items-center bg-[#ecf03c]">
+            <h1 class="py-1 lg:py-0 font-title text-[40px] md:text-[45px] lg:text-[55px] tracking-widest">
                 <a href="<?php echo esc_url(home_url('/')); ?>">
                     <?php bloginfo('name'); ?>
                 </a>
             </h1>
 
-            <nav class="mb-0 md:w-3/5 md:text-xl">
-                <ul class="flex justify-between text-left">
-                    <li class="w-[42px] md:w-[52px]">
-                        <a class="h-nav hover:text-yellow-500 focus:text-yellow-500 border-yellow-500" 
+            <nav class="mb-0 mx-auto lg:mx-0 lg:w-1/2 text-sm md:text-xl lg:text-2xl">
+                <ul class="mx-auto w-[95%] lg:w-full flex justify-between">
+                    <li>
+                        <a class="h-nav"
                             href="<?php home_url(); ?>/menu">
                             Menu
                         </a>
                     </li>
-                    <li class="w-[40px] md:w-[50px]">
-                        <a class="h-nav hover:text-green-600 focus:text-green-600 border-green-500" 
+                    <li>
+                        <a class="h-nav"
                             href="<?php home_url(); ?>/news">
                             News
                         </a>
                     </li>
-                    <li class="w-[32px] md:w-[40px]">
-                        <a class="h-nav hover:text-violet-600 focus:text-violet-600 border-violet-500" 
+                    <li>
+                        <a class="h-nav" 
                             href="<?php home_url(); ?>/blog">
                             Blog
                         </a>
                     </li>
-                    <li class="w-[49px] md:w-[61px]">
-                        <a class="h-nav hover:text-orange-600 focus:text-orange-600 border-orange-500" 
+                    <li>
+                        <a class="h-nav" 
                             href="<?php home_url(); ?>/access">
                             Access
                         </a>
                     </li>
-                    <li class="w-[69px] md:w-[86px]">
-                        <a class="h-nav hover:text-cyan-500 focus:text-cyan-500 border-cyan-500" 
+                    <li>
+                        <a class="h-nav" 
                             href="<?php home_url(); ?>/company">
                             Company
                         </a>
                     </li>
-                    <li class="w-[56px] md:w-[70px]">
-                        <a class="h-nav hover:text-pink-600 focus:text-pink-600 border-pink-500" 
+                    <li>
+                        <a class="h-nav" 
                             href="<?php home_url(); ?>/contact">
                             Contact
                         </a>
@@ -61,29 +61,31 @@
         <?php if (is_front_page()): ?>
         <?php else: ?>
             <?php if(is_post_type_archive('menu') || is_singular('menu')): ?>
-                <div class="page-head bg-gradient-to-r from-yellow-50 to-yellow-400">
-                    <div class="page-ttl">
-                        <h3 class="ttl-up -rotate-6 -top-8 md:-top-10 text-yellow-500">
+                <div class="page-head">
+                    <div class="page-ttl w-[270px] md:w-[420px]">
+                        <h3 class="ttl-up -rotate-[7deg] bottom-10 md:bottom-2">
                             Menu
                         </h3>
                         <h2 class="ttl-down">
                             メニュー
                         </h2>
                     </div>
+                </div>
             <?php elseif(is_post_type_archive('news') || is_singular('news')): ?>
-                <div class="page-head bg-gradient-to-r from-green-50 to-green-500">
-                    <div class="page-ttl">
-                        <h3 class="ttl-up -rotate-6 -top-8 md:-top-10 text-green-600">
+                <div class="page-head">
+                    <div class="page-ttl w-[250px] md:w-[390px]">
+                        <h3 class="ttl-up -rotate-6 bottom-10 md:bottom-2">
                             News
                         </h3>
                         <h2 class="ttl-down">
                             ニュース
                         </h2>
                     </div>
+                </div>
             <?php elseif(is_archive() || is_single()): ?>
-                <div class="page-head bg-gradient-to-r from-violet-50 to-violet-500">
-                    <div class="page-ttl">
-                        <h3 class="ttl-up -rotate-12 -top-9 md:-top-11 text-violet-600">
+                <div class="page-head">
+                    <div class="page-ttl w-[240px] md:w-[350px]">
+                        <h3 class="ttl-up -rotate-12 bottom-8 md:bottom-2">
                             Blog
                         </h3>
                         <h2 class="ttl-down">
@@ -91,9 +93,9 @@
                         </h2>
                     </div>
             <?php elseif(is_page(7)): ?>
-                <div class="page-head bg-gradient-to-r from-orange-50 to-orange-400">
-                    <div class="page-ttl">
-                        <h3 class="ttl-up -rotate-6 -top-8 md:-top-11 text-orange-500">
+                <div class="page-head">
+                    <div class="page-ttl w-[290px] md:w-[440px]">
+                        <h3 class="ttl-up -rotate-6 bottom-10 md:bottom-2">
                             Access
                         </h3>
                         <h2 class="ttl-down">
@@ -101,22 +103,22 @@
                         </h2>
                     </div>
             <?php elseif(is_page(9)): ?>
-                <div class="page-head bg-gradient-to-r from-cyan-50 to-cyan-400">
-                    <div class="page-ttl">
-                        <h3 class="ttl-up -rotate-6 -top-10 md:-top-12 text-cyan-500">
+                <div class="page-head">
+                    <div class="page-ttl w-[310px] md:w-[510px]">
+                        <h3 class="ttl-up text-[80px] md:text-[120px] -rotate-[7deg] bottom-10 md:bottom-3">
                             Company
                         </h3>
-                        <h2 class="ttl-down">
+                        <h2 class="ttl-down text-[30px] md:text-[43px] tracking-widest right-10">
                             会社概要
                         </h2>
                     </div>
             <?php elseif(is_page(11)): ?>
-                <div class="page-head bg-gradient-to-r from-pink-50 to-pink-400">
-                    <div class="page-ttl">
-                        <h3 class="ttl-up -rotate-6 -top-8 md:-top-11 text-pink-500">
+                <div class="page-head">
+                    <div class="page-ttl w-[280px] md:w-[420px]">
+                        <h3 class="ttl-up text-[80px] md:text-[120px] -rotate-6 left-4 bottom-9 md:bottom-2">
                             Contact
                         </h3>
-                        <h2 class="ttl-down">
+                        <h2 class="ttl-down text-[30px] md:text-[43px]">
                             お問い合わせ
                         </h2>
                     </div>
